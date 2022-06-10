c95f898c33a831e762f55e85f238865bb79b063a
Loryn <lorynhaykus@yahoo.com>
2022-06-10 00:38:21 -0600

    added scroll component from another project of mine and styling products page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import HeroImage from './components/HeroImage';
 import ProductList from './pages/ProductList';
 import Footer from './components/Footer';
 import IndividualProduct from './pages/IndividualProduct';
-import LearnMore from './pages/LearnMore';
+import LearnMore from './pages/LearnMore'; 
+import Scroll from './components/Scroll';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
        <Route path = '/learnmore' element={<LearnMore />} />
      </Routes>
      <Footer />
+     <Scroll showBelow={250} />
   
     </div>
   );
