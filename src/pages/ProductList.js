f5a6f7eee803231b4c2dd9e9dda5382a30196867
Loryn <lorynhaykus@yahoo.com>
2022-06-09 23:26:39 -0600

    creating performacne parts filter page

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import Products from '../components/Products';
 import './ProductList.css';
 
 
 const ProductList = () => {
   return (
     <section>
+        <div className='performance-hero-image'></div>
         <h1>Performance Parts</h1>
         <div className='filter-container'>
-            <p>Filter</p>
-            <p>Filter</p>
-
         </div>
+        <Products />
+        
   
     </section>
   )
