fd910f2f3917a1b849094574f3026d49104185f8
Loryn <lorynhaykus@yahoo.com>
2022-06-09 20:38:10 -0600

    chanign layout of categories

--- src/components/CatergoryItem.js
+++ src/components/CatergoryItem.js
@@ -4,16 +4,17 @@ import { Button } from '@material-ui/core';
 
 
 const CatergoryItem = ({ item }) => {
-  return (
-   <div>
-    <div className='category-title'>{item.title}</div>
-    <img src={item.img} className='category-img'/>
-    <br /><br />
-    <div>
-        <Button className='category-btn'>Shop Now</Button>
-    </div>
-   </div>
-  )
+    return (
+        <div>
+
+            <img src={item.img} className='category-img' />
+            <br /><br />
+            <div>
+                <div className='category-title'>{item.title}</div>
+                <Button className='category-btn'>Shop Now</Button>
+            </div>
+        </div>
+    )
 }
 
 export default CatergoryItem;
--- src/data.js
+++ src/data.js
@@ -2,16 +2,16 @@ export const categories = [
     {
         id: 1,
         img: 'https://images.rallysportdirect.com/image/private/s--js6He5X5--/f_auto,t_auto_rsd_product/v1436911214/product_images/tom_440006_1',
-        title: 'Performance Exhaust',
+        title: ' JDM Performance Parts',
     },
     {
         id: 2,
-        img: 'https://m.media-amazon.com/images/I/71EVqYpOHFL._AC_SL1500_.jpg',
-        title: 'Sport Rotor',
+        img: 'https://visracing.com/images/M198738582',
+        title: 'Accessories',
     },
     {
         id: 3,
-        img: 'https://www.autoguide.com/blog/wp-content/uploads//2018/09/pilot-sport-4s-product-page-tire-image-495x468-2.png',
-        title: 'Summer Performance Tires',
+        img: 'https://res.cloudinary.com/teepublic/image/private/s--itpkhzjs--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_0/g_north_west,u_upload:v1462829024:production:blanks:a59x1cgomgu5lprfjlmi,x_-395,y_-325/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1539812435/production/designs/3338373_0.jpg',
+        title: 'Merch',
     },
 ];
