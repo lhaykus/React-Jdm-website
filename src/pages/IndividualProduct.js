2a47535bda1bfddfba078c867376c585fbff4923
Loryn <lorynhaykus@yahoo.com>
2022-06-13 18:42:07 -0600

    creating individual product page and styling

--- src/pages/IndividualProduct.js
+++ src/pages/IndividualProduct.js
@@ -6,17 +6,29 @@ import './IndividualProduct.css';
 const IndividualProduct = () => {
   return (
     <section className='ind-product'>
-        <div className='img-container'>
-            <img className='indiv-img' src={rotor} />
-            <div className='info-container'>
-                <h1>ehkaklfjhe</h1>
-                <div className='description-container'>
-                    jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksfjsfgjls;ldkfg
-                    jlksdfgjlk;sdglksdfj;ldks
-                    jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksf 
-                    jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksf f  </div>
-            </div>
+      <div className='img-container'>
+        <img className='indiv-img' src={rotor} />
+        <div className='info-container'>
+          <h1>Sport Performance Rotor</h1>
+          <p className='description-container'>
+            jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksfjsfgjls;ldkfg
+            jlksdfgjlk;sdglksdfj;ldks
+            jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksf
+            jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksf f jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksfjsfgjls;ldkfg
+            jlksdfgjlk;sdglksdfj;ldks
+            jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksf
+            jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksf f jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksfjsfgjls;ldkfg
+            jlksdfgjlk;sdglksdfj;ldks
+            jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksf
+            jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksf f jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksfjsfgjls;ldkfg
+            jlksdfgjlk;sdglksdfj;ldks
+            jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksf
+            jsfgjls;ldkfgjlksdfgjlk;sdglksdfj;ldksf f  </p>
+            <p>$280</p>
         </div>
+
+      </div>
+     
 
     </section>
   )
