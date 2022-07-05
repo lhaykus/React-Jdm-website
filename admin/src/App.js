0f666ff49de19a75941c4d08fcf030e230e123cc
Loryn <lorynhaykus@yahoo.com>
2022-07-05 16:53:46 -0600

    added create user on userlist and now crateing product list

--- admin/src/App.js
+++ admin/src/App.js
@@ -7,6 +7,7 @@ import {HashRouter as Router, Routes, Route, Redirect,} from "react-router-dom";
 import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
+import ProductList from './pages/productList/ProductList';
 
 
 
@@ -22,6 +23,9 @@ const App = () => {
             <Route exact path="/users" element = {<UserList />}/>
             <Route exact path="/user/:userId" element = {<User />}/>
             <Route exact path="/newUser" element = {<NewUser />}/>
+            <Route exact path="/products" element = {<ProductList />}/>
+            <Route exact path="/products/:productsId" element = {<User />}/>
+            <Route exact path="/newProduct" element = {<NewUser />}/>
           </Routes>
         </div>
       </Router>
--- admin/src/pages/userList/UserList.jsx
+++ admin/src/pages/userList/UserList.jsx
@@ -7,78 +7,83 @@ import { Link } from "react-router-dom";
 
 
 const UserList = () => {
-    const [data, setData] = useState(userRows);
+  const [data, setData] = useState(userRows);
 
-    const handleDelete = (id) => {
-      setData(data.filter((item) => item.id !== id));
-    };
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
 
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
-        {
-            field: 'user',
-            headerName: 'User',
-            width: 200,
-            //rendering an avatar from the data in rows 
-            renderCell: (params) => {
-                return (
-                  <div className="userlist-user">
-                    <img className="userlist-img" src={params.row.avatar} alt="" />
-                    {params.row.username}
-                  </div>
-                );
-              },
-            },
-        {
-            field: 'email',
-            headerName: 'Email',
-            width: 200,
-            editable: true,
-        },
-        {
-            field: 'status',
-            headerName: 'Status',
-            width: 200,
-            editable: true,
-        },
-        {
-            field: 'transaction',
-            headerName: 'Transaction Volume',
-            width: 200,
-        },
-        {
-            field: "action",
-            headerName: "Action",
-            width: 200,
-            renderCell: (params) => {
-              return (
-                <>
-                  <Link to={"/user/" + params.row.id}>
-                    <button className="userListEdit">Edit</button>
-                  </Link>
-                  <DeleteOutline
-                    className="userlist-delete"
-                    onClick={() => handleDelete(params.row.id)}
-                  />
-                </>
-              );
-            },
-          },
-    ];
-
-    
-
-    return (
-        <div className='user-list'>
-            <DataGrid
-                rows={data}
-                columns={columns}
-                pageSize={5}
-                checkboxSelection
-                disableSelectionOnClick
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'user',
+      headerName: 'User',
+      width: 200,
+      //rendering an avatar from the data in rows 
+      renderCell: (params) => {
+        return (
+          <div className="userlist-user">
+            <img className="userlist-img" src={params.row.avatar} alt="" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      width: 200,
+      editable: true,
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 200,
+      editable: true,
+    },
+    {
+      field: 'transaction',
+      headerName: 'Transaction Volume',
+      width: 200,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id}>
+              <button className="userListEdit">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="userlist-delete"
+              onClick={() => handleDelete(params.row.id)}
             />
-        </div>
-    )
+          </>
+        );
+      },
+    },
+  ];
+
+
+
+  return (
+
+    <div className='user-list'>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={5}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+      <Link to='/newUser'>
+      <button className="create-user-btn">Create New User</button>
+      </Link>
+    </div>
+
+  )
 }
 
 export default UserList;
