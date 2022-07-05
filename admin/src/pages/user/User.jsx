e870b12c55012918b12e4eadb37ea3dd28184f58
Loryn <lorynhaykus@yahoo.com>
2022-07-05 16:29:01 -0600

    creating ability to edit user in admin panel

--- admin/src/pages/user/User.jsx
+++ admin/src/pages/user/User.jsx
@@ -1,4 +1,4 @@
-import { Cake, Call, MailOutline, People, Map } from '@material-ui/icons';
+import { Cake, Call, MailOutline, People, Map, Publish } from '@material-ui/icons';
 import React from 'react';
 import './user.css';
 
@@ -43,18 +43,74 @@ const User = () => {
                             <Map className='user-icon' />
                             <span className='show-user-text'>Colorado | USA</span>
                         </div>
-
-
                     </div>
                 </div>
 
-                <div className='update-user'> </div>
-
-
-
+                <div className='update-user'>
+                    <div className='update-user-title'>Edit</div>
+                    <form className='update-user-form'>
+                        <div className='update-user-left'>
+                            <div className='update-user-item'>
+                                <label>Username</label>
+                                <input
+                                    type="text"
+                                    placeholder="username"
+                                    className="update-user-input"
+                                />
+                            </div>
+                            <div className="update-user-item">
+                                <label>Full Name</label>
+                                <input
+                                    type="text"
+                                    placeholder="Jon Snow"
+                                    className="update-user-input"
+                                />
+                            </div>
+                            <div className="update-user-item">
+                                <label>Email</label>
+                                <input
+                                    type="text"
+                                    placeholder="jonsnow@gmail.com"
+                                    className="update-user-input"
+                                />
+                            </div>
+                            <div className="update-user-item">
+                                <label>Phone</label>
+                                <input
+                                    type="text"
+                                    placeholder="123 456 789"
+                                    className="update-user-input"
+                                />
+                            </div>
+                            <div className="update-user-item">
+                                <label>Address</label>
+                                <input
+                                    type="text"
+                                    placeholder="Colorado | USA"
+                                    className="update-user-input"
+                                />
+                            </div>
+                            <div className="update-user-right">
+                                <div className="update-user-upload">
+                                    <img
+                                        className="update-user-img"
+                                        src="https://images.pexels.com/photos/1571882/pexels-photo-1571882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                        alt=""
+                                    />
+                                    <label htmlFor="file">
+                                        <Publish className="update-user-icon" />
+                                    </label>
+                                    <input type="file" id="file" style={{ display: "none" }} />
+                                </div>
+                                <button className="update-btn">Update</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
 
             </div>
-        </div>
+        </div >
+
     )
 }
 
