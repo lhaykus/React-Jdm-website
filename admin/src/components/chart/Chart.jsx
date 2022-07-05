5d3a6a3c81742b4a66e62d5e9c54b18b0cb7aca0
Loryn <lorynhaykus@yahoo.com>
2022-07-04 18:35:17 -0600

    creating chart component

---
+++ admin/src/components/chart/Chart.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './chart.css';
+
+const Chart = () => {
+  return (
+    <div className='chart'>Chart</div>
+  )
+}
+
+export default Chart;
--- admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -9,11 +9,33 @@ const FeaturedInfo = () => {
             <span className='featured-title'>Revanue</span>
             <div className='featured-money-container'>
                 <span className='featured-money'>$24120</span>
-                <ArrowDownward />
-                <ArrowUpward />
+                <span className='money-direction'> +3.0
+                <ArrowUpward  className='featured-icon'/>
+                </span>
+        
+                
             </div>
-
-        </div>
+            </div>
+            <div className='featured-item'>
+            <span className='featured-title'>Sales</span>
+            <div className='featured-money-container'>
+                <span className='featured-money'>$24120</span>
+                <span className='money-direction'> +3.0
+                <ArrowUpward  className='featured-icon'/>
+                </span>
+           
+            </div>
+       </div>
+       <div className='featured-item'>
+            <span className='featured-title'>Price</span>
+            <div className='featured-money-container'>
+                <span className='featured-money'>$24120</span>
+                <span className='money-direction'> +3.0
+                <ArrowDownward  className='featured-icon negative'/>
+                </span>
+          
+            </div>
+       </div>
     </div>
   )
 }
